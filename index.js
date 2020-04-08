25bd77b5f9e6554b0412fd3e6b6efe476c8ec58a
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 18:31:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function wakeDog(a, b) {
-  return s
+  return `Wake ${a} the ${b}`
+}
+function leashDog(a, b) {
+  return `Leash ${a} the ${b}`
+}
+function walkToPark(a, b) {
+  return `Walk to the park with ${a} the ${b}`
+}
+function throwFrisbee(a, b) {
+  return `Throw the frisbee for ${a} the ${b}`
+}
+function walkHome(a, b) {
+  return `Walk home with ${a} the ${b}`
+}
+function unleashDog(a, b) {
+  return `Unleash ${a} the ${b}`
 }
