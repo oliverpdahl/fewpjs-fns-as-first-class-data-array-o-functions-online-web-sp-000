abe379c43486967b73e7a3dfad033955c07937e3
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 18:36:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function wakeDog(a, b) {
+  return `Wake ${a} the ${b}`
+}
+function leashDog(a, b) {
+  return `Leash ${a} the ${b}`
+}
+function walkToPark(a, b) {
+  return `Walk to the park with ${a} the ${b}`
+}
+function throwFrisbee(a, b) {
+  return `Throw the frisbee for ${a} the ${b}`
+}
+function walkHome(a, b) {
+  return `Walk home with ${a} the ${b}`
+}
+function unleashDog(a, b) {
+  return `Unleash ${a} the ${b}`
+}
+
+const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+
+function exerciseDog(a,b) {
+  let successMessages = [];
+  for(let i = 0; i < routine.length; i++) {
+    successMessages.push(routine[i](a,b));
+  }
+  return successMessages
+}
